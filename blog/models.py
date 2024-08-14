3c4d0b8a5c384d8a021ec947041f074be5cd755e
soni-maurya17 <sonimaurya1711@gmail.com>
2024-08-14 16:15:54 +0530

    first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class article(models.Model):
+    author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    title=models.CharField(max_length = 150)
+    Content=models.TextField()
+    image=models.ImageField(upload_to = "articles/")
+    created_at=models.DateTimeField(auto_now_add=True)
+    
---
+++ main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+def login_view(request):
+    if request.method =='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user =authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('index')
+        print("welcome",username)
+    return render(request,'login.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if password == cpassword:
+            user= User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect('login')
+    return render(request,'register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index.html')
+
